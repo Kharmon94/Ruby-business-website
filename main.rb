4e71fdbe1b1776a1acfe3d4fbc4d09fa0e8fd630
Joel Jester <feralseattle@gmail.com>
2017-02-08 12:19:43 -0500

    more layout added for portfolio page

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
-require 'geolocater'
-require 'rainbow'
+# require 'geolocater'
+# require 'rainbow'
 
-set :nav_buttons, [{title: "Home", route: '/'}, {title: "About", route: '/about'}, {title: "Portfolio", route: '/portfolio'}, {title: "Contact", route: '/contact'}]
+set :nav_buttons, [ {title: "Contact", route: '/contact'}, {title: "Portfolio", route: '/portfolio'}, {title: "About", route: '/about'}, {title: "Home", route: '/'}]
 
 
 get '/' do
