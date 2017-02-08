a4e89be8b8e7152985c5f1e72f442ed46a4dec6a
smplyamzng <kharmon9452@gmail.com>
2017-02-08 13:08:21 -0500

    responsive changes

--- main.rb
+++ main.rb
@@ -6,24 +6,25 @@ set :nav_buttons, [{title: "Home", route: '/'}, {title: "About", route: '/about'
 
 
 get '/' do
-
+	@title = "Home"
 	erb :index
 
 end
 
 get '/about' do
-
+	@title = "About"
 	erb :about
 
 end
 
 get '/portfolio' do
-
+	@title = "Portfolio"
 	erb :portfolio
 	# erb :index
 end
 
 get '/contact' do
+	@title = "Contact"
 	erb :contact
 	# erb :index
 end
