bb57590ae1142e5c41673c330e6d44611404da2c
Joel Jester <feralseattle@gmail.com>
2017-02-10 13:58:48 -0500

    form built and javascript for portfolio page

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'geolocater'
-require 'SendGrid'
+require 'sendgrid-ruby'
 
 
 set :nav_buttons, [ {title: "Contact", route: '/contact'}, {title: "Portfolio", route: '/portfolio'}, {title: "About", route: '/about'}, {title: "Home", route: '/'}]
@@ -28,5 +28,20 @@ get '/contact' do
 	@title = "Contact"
 	erb :contact
 	# erb :index
+end
+
+post '/contact' do
+	from = SendGrid::Email.new email: params[:email]
+	to = SendGrid::Email.new email: "xcatlkd@gmail.com"
+	subject = params[:subject]
+	content = SendGrid::Content.new(
+			type: 'plain/text',
+			value: params[:comment]
+		)
+	mail = SendGrid::Mail.new(from, subject, to, content)
+	sg = SendGrid::API.new( api_key: ENV['SENDGRID_API_KEY'])
+
+	response = sg.client.mail_('send').post(request_body: mail.to_json)
+	response.inspect
 end
 
